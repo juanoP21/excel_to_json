396c0eed41b47cecd6736767d77d2ceda0d16d73
Juan Manuel Perlaza Perez <122650328+juanoP21@users.noreply.github.com>
2025-07-14 15:45:58 -0500

    Switch to Postgres database

--- excel_to_json/settings.py
+++ excel_to_json/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = 'excel_to_json.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'excel_to_json'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
