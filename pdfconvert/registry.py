2c64298a369bdf48bca5675d9b7d6296c40ff705
Juan Manuel <juanmanuelperlazaperez@gmail.com>
2025-07-17 09:03:08 -0500

    Fix TextractParser to use the correct parse function for bancolombia_textract

--- pdfconvert/registry.py
+++ pdfconvert/registry.py
@@ -19,6 +19,8 @@ from pdfconvert.utils.parse_casa_bolsa import (
     parse_casa_bolsa_transformado,
 )
 
+from pdfconvert.parsers.textract import parse_func
+
 HANDLERS = {
     'bancolombia': {
         'parser':    ParserBancolombia(parse_func=parse_bancolombia),
@@ -33,7 +35,7 @@ HANDLERS = {
         'serializer': None,
     },
     'bancolombia_textract': {
-        'parser':    TextractParser(parse_func=parse_bancolombia_transformado),
+        'parser':    TextractParser(parse_func=parse_func),
         'serializer': None,
     },
     'davivienda_transformado': {
